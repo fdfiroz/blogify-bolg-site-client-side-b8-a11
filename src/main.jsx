11885ab017d36299ff5b360bc11aaf10c24d89a3
fdfiroz <firozahamed0@gmail.com>
2023-11-06 15:12:10 +0600

    Add react-data-table-component and react-query

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,19 @@ import routes from './routes'
 import { ThemeProvider } from "@material-tailwind/react";
 import AuthProvider from './providers/AuthProvider'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <RouterProvider router={routes} />
       <Toaster/>
     </AuthProvider>
+    </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
