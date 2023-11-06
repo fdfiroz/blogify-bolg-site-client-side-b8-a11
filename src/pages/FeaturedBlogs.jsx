17edf80c6c04218331435f761039c532f623e480
fdfiroz <firozahamed0@gmail.com>
2023-11-06 19:33:28 +0600

    Add featured blogs table with data and loading state

--- src/pages/FeaturedBlogs.jsx
+++ src/pages/FeaturedBlogs.jsx
@@ -1,7 +1,62 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxios from "../hooks/useAxios"
+import DataTable from "react-data-table-component"
+import { Avatar } from "@material-tailwind/react";
+import Loading from "../components/Loading/Loading";
 
 const FeaturedBlogs = () => {
+  const axios = useAxios()
+  const { isLoading, data } = useQuery({
+    queryKey: ['blogs'],
+    queryFn: async () =>{
+      const res = await axios.get('/featured-blogs  ')
+      return res.data
+  }})
+  console.log(data);
+  const columns = [
+    {
+      name: 'Serial No',
+      selector: (row, index) => <span>{index+1}</span>,
+  },
+  {
+        name: 'Title',
+        selector: row => row.title,
+    },
+    {
+      name: 'Author',
+      selector: row => row.author,
+  },
+  {
+    name: 'Profile Image',
+    selector: row => <Avatar
+    src={row?.original?.image}
+    alt={row?.original?.author}
+    withBorder={true}
+    className="p-0.5"
+  />,
+},
+];
+
+
   return (
-    <div>FeaturedBlogs</div>
+    <>
+    <div className="my-6">
+    {
+      isLoading ? <Loading /> : (<DataTable
+        title="Featured Blogs"
+        columns={columns}
+        data={data}
+        highlightOnHover
+     
+        // fixedHeader
+         
+        // selectableRows
+        // selectableRowsComponent={Checkbox} // Pass the function only
+        
+      />)
+    }
+    </div>
+    </>
   )
 }
 
